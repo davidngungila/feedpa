9bb84b2429e5d21409f2cefdffa6fcf2bd6a258d
David Ngungila <davidngungila@gmail.com>
2026-05-30 12:02:24 +0300

    Add missing AuditController import in web.php routes file

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CallbackController;
 use App\Http\Controllers\BillController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuditController;
 
 /*
 |--------------------------------------------------------------------------

350f79c8be9d59cc7cba030a69453a787b67f6f2
David Ngungila <davidngungila@gmail.com>
2026-06-25 10:21:00 +0300

    Fix beneficiary creation by adding user_id to fillable attributes

--- app/Models/Beneficiary.php
+++ app/Models/Beneficiary.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['name', 'type', 'phone', 'bank_name', 'account_number', 'bic', 'transfer_type', 'email', 'is_active'])]
+#[Fillable(['name', 'type', 'phone', 'bank_name', 'account_number', 'bic', 'transfer_type', 'email', 'is_active', 'user_id'])]
 class Beneficiary extends Model
 {
     use HasFactory;

cc56c2e3aa495c7974fa68a30297e84c968e0a1f
David Ngungila <davidngungila@gmail.com>
2026-08-04 16:07:25 +0300

    Fix LoginController import missing in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\PayoutController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\WhatsAppSettingsController;
 use App\Http\Controllers\AiChatController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
